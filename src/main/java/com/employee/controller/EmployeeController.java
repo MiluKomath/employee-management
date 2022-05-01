244ea7ab0c95e1a19b31bb987ce399ba9ae41668
Milu <mks.c@saip.gov.sa>
2022-05-01 22:37:30 +0530

    CRUD commit

--- src/main/java/com/employee/controller/EmployeeController.java
+++ src/main/java/com/employee/controller/EmployeeController.java
@@ -1,5 +1,49 @@
 package com.employee.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.employee.model.Employee;
+import com.employee.service.EmployeeService;
+
+@RestController
 public class EmployeeController {
+	@Autowired
+	EmployeeService employeeService;
+
+	@GetMapping("/employee")
+	private List<Employee> getAllEmployee() {
+		return employeeService.getAllEmployee();
+	}
+
+	@GetMapping("/employee/{id}")
+	private Employee getEmployee(@PathVariable("id") int id) {
+		return employeeService.getEmployeeById(id);
+	}
+
+	@DeleteMapping("/employee/{id}")
+	private void deleteEmployee(@PathVariable("id") int id) {
+		employeeService.delete(id);
+	}
+
+	@PutMapping("/employee/{id}")
+	private int updateEmployee(@RequestBody Employee employee) {
+		employeeService.saveOrUpdate(employee);
+		return employee.getId();
+
+	}
+
+	@PostMapping("/employee")
+	private int saveEmployee(@RequestBody Employee employee) {
+		employeeService.saveOrUpdate(employee);
+		return employee.getId();
+
+	}
 
 }
--- src/main/java/com/employee/service/EmployeeService.java
+++ src/main/java/com/employee/service/EmployeeService.java
@@ -4,33 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Service;
 import com.employee.model.Employee;
 import com.employee.repository.EmployeeRepository;
 
+@Service
 public class EmployeeService {
-	@Autowired  
-	EmployeeRepository employeeRepository;  
-	
-	public List<Employee> getAllEmployee()   
-	{  
-	List<Employee> employees = new ArrayList<Employee>();  
-	employeeRepository.findAll().forEach(employee -> employees.add(employee));  
-	return employees;  
-	}  
+	@Autowired
+	EmployeeRepository employeeRepository;
 
-	public Employee getStudentById(int id)   
-	{  
-	return employeeRepository.findById(id).get();  
-	}  
-	public void saveOrUpdate(Employee employee)   
-	{  
-	employeeRepository.save(employee);  
-	}  
-	//deleting a specific record  
-	public void delete(int id)   
-	{  
-	employeeRepository.deleteById(id);  
-	}  
+	public List<Employee> getAllEmployee() {
+		List<Employee> employees = new ArrayList<Employee>();
+		employeeRepository.findAll().forEach(employee -> employees.add(employee));
+		return employees;
+	}
+
+	public Employee getEmployeeById(int id) {
+		return employeeRepository.findById(id).get();
+	}
+
+	public void saveOrUpdate(Employee employee) {
+		employeeRepository.save(employee);
+	}
+
+	public void delete(int id) {
+		employeeRepository.deleteById(id);
+	}
 
 }
